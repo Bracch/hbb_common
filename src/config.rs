1914ca4b5641e257652cb1fb5990b102d9c39265
Nicolas Dissoux <65357509+Bracch@users.noreply.github.com>
2026-05-07 13:22:46 +0200

    Update rendezvous server and public key constants

--- src/config.rs
+++ src/config.rs
@@ -156,8 +156,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.comlink.fr"];
+pub const RS_PUB_KEY: &str = "7g7ksJLudhxl6lrqpXY6AwwSMjpma4gMtWM3h7PQt1E=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
